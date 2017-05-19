c3e32c721a964ccb6d19ef5eb4f574217a6aa7cc
GaneshSPatil <ganeshpl@thoughtworks.com>
2017-05-19 22:10:40 +0530

    Moved the variables to the related function to cleanup the headers section

--- test/exampleTest.js
+++ test/exampleTest.js
@@ -1,26 +1,26 @@
 var assert = require('assert');
 var lib = require('../src/example.js');
 
-var add = lib.add;
-var subtract = lib.subtract;
-var multiply = lib.multiply;
-var divide = lib.divide;
-
 var test = {};
 
 test["add should add two numbers"] = function () {
+  var add = lib.add;
   assert.equal(add(2, 3), 5);
 };
 
 test["subtract should subtract one number from another"] = function () {
+  var subtract = lib.subtract;
   assert.equal(subtract(2, 3), -1);
 };
 
 test["multiply should multiply two numbers"] = function () {
+  var multiply = lib.multiply;
   assert.equal(multiply(2, 3), 6);
 };
 
 test["divide should divide one number from another"] = function () {
+  var divide = lib.divide;
   assert.equal(divide(6, 3), 2);
 };
+
 exports.test = test;
