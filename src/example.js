d8b42cef785f90cc221b4fb10d157afd2fd55d7d
GaneshSPatil <ganeshpl@thoughtworks.com>
2017-05-19 20:17:24 +0530

    Add few more tests

--- src/example.js
+++ src/example.js
@@ -4,4 +4,16 @@ lib.add = function(num1, num2) {
     return num1 + num2;
 };
 
+lib.subtract = function(num1, num2) {
+    return num1 - num2;
+};
+
+lib.multiply = function(num1, num2) {
+    return num1 * num2;
+};
+
+lib.divide = function(num1, num2) {
+    return num1 / num2;
+};
+
 module.exports = lib;
--- test/exampleTest.js
+++ test/exampleTest.js
@@ -1,11 +1,26 @@
 var assert = require('assert');
 var lib = require('../src/example.js');
+
 var add = lib.add;
+var subtract = lib.subtract;
+var multiply = lib.multiply;
+var divide = lib.divide;
 
 var test = {};
 
 test["add should add two numbers"] = function () {
   assert.equal(add(2, 3), 5);
-}
+};
 
+test["subtract should subtract one number from another"] = function () {
+  assert.equal(subtract(2, 3), -1);
+};
+
+test["multiply should multiply two numbers"] = function () {
+  assert.equal(multiply(2, 3), 6);
+};
+
+test["divide should divide one number from another"] = function () {
+  assert.equal(divide(6, 3), 2);
+};
 exports.test = test;
